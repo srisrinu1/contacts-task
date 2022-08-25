333c758171991931c5dff6903b1f6512cbcc035b
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-08-26 00:09:52 +0530

    Added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     let [open,setOpen]=useState(false);
   return (
 
-    <div className='shadow-md w-full fixed top-0 left-0'>
+    <div className='shadow-md w-full sticky top-0 left-0 '>
       <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
       <div className='font-bold text-2xl cursor-pointer flex items-center
       text-gray-800'>
