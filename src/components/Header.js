26d26d495a49fe628d66ba79d291b65b8fe1444f
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-08-28 01:41:58 +0530

    Added search functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
 import React,{useState} from 'react';
+import { useDispatch} from 'react-redux';
 import {AiOutlineMenu} from 'react-icons/ai';
+import { filterContacts } from '../actions/contacts-action';
 
 const Header = () => {
     let [open,setOpen]=useState(false);
+    const [search,setSearch]=useState('');
+    const dispatch=useDispatch();
+    console.log(search)
+    const handleSubmit=(event)=>{
+      event.preventDefault()
+      console.log(search)
+      dispatch(filterContacts(search))
+    }
   return (
 
     <div className='shadow-md w-full sticky top-0 left-0 '>
@@ -21,12 +31,14 @@ const Header = () => {
       </div>
 
 
-        <div className={`md:flex md:items-center md:pb-0 pb-12 space-x-2 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all  duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+        <div className={`md:flex md:items-center md:pb-0 pb-12 space-x-2 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full   md:w-auto md:pl-0 pl-9 transition-all  duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
 
-          <input type="text" className='bg-gray-200 p-2 px-6 rounded-lg focus:outline-none' />
-         <button className='bg-gray-200 p-2 px-4 rounded-lg'>
+            <form className='space-x-2' onSubmit={handleSubmit}>
+          <input type="text" className='bg-gray-200 p-2 px-6 rounded-lg focus:outline-none' value={search} onChange={(e)=>setSearch(e.target.value)} />
+         <button type='submit' className='bg-gray-200 p-2 px-4 rounded-lg'>
             Search
          </button>
+         </form>
 
         </div>
 
